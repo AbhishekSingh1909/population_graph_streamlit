c2f29a6d2ca8267e6de540546496be755d57c962
AbhishekSingh1909 <103984974+AbhishekSingh1909@users.noreply.github.com>
2024-09-28 08:28:48 +0300

    Add files via upload
    add initial files to draw graph

---
+++ DataAnalyticsClass/population_graph.py
@@ -0,0 +1,34 @@
+# make graphics
+
+import streamlit as st
+import pandas as pd
+
+# define figure title
+st.title('Population Plot')
+
+# Read the CSV file
+df = pd.read_csv("population.csv")
+
+# Drop unnecessary columns if any (e.g., 'Unnamed: 0')
+if 'Unnamed: 0' in df.columns:
+    df = df.drop(columns='Unnamed: 0')
+
+# Pivot the DataFrame to have years as rows and countries as columns
+df_population = df.pivot(index='year', columns='country', values='pop')
+
+
+# Reset index to have 'year' as a column
+df_population = df_population.reset_index()
+
+# Rename columns to remove multi-level headers (i.e., remove 'country' label)
+# This removes the 'country' name from the columns
+df_population.columns.name = None
+
+countries = df['country'].unique().tolist()
+
+# define selectors(column to draw)
+
+column = st.multiselect('Countries', countries)
+# plot the line chart
+st.line_chart(df_population, x='year', y=column,
+              x_label='year', y_label='Population')
