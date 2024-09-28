1768366970b14c9531272c0b961aadcc71dcac3a
AbhishekSingh1909 <103984974+AbhishekSingh1909@users.noreply.github.com>
2024-09-28 23:43:21 +0300

    Update population_graph.py

--- DataAnalyticsClass/population_graph.py
+++ DataAnalyticsClass/population_graph.py
@@ -7,7 +7,8 @@ import pandas as pd
 st.title('Population Plot')
 
 # Read the CSV file
-df = pd.read_csv("./population.csv")
+url = "https://github.com/AbhishekSingh1909/population_graph_streamlit/blob/main/DataAnalyticsClass/population.csv"
+df = pd.read_csv(url)
 
 # Drop unnecessary columns if any (e.g., 'Unnamed: 0')
 if 'Unnamed: 0' in df.columns:
